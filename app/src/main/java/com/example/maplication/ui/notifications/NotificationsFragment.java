e27ee21cdb784f3f7052d21dcd47a3da2aec53db
Brett Presscott <brettpresscott@icloud.com>
2022-11-11 22:19:51 +0200

    v0.1 Settings page added

--- app/src/main/java/com/example/maplication/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/maplication/ui/notifications/NotificationsFragment.java
@@ -4,17 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.maplication.R;
 import com.example.maplication.databinding.FragmentNotificationsBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class NotificationsFragment extends Fragment {
+public class NotificationsFragment extends Fragment{
 
     private FragmentNotificationsBinding binding;
+
+    String distanceScale;
+    String landmarksType;
+    String darkMode;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +33,54 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        if (binding.darkModeSwitch.isChecked())
+        {
+            darkMode = "dmon";
+        }
+        else
+        {
+            darkMode = "dmoff";
+        }
+
+
+        ArrayAdapter<CharSequence> distanceAdapter = ArrayAdapter.createFromResource(getContext(), R.array.distances, android.R.layout.simple_spinner_item);
+        distanceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.distanceSpinner.setAdapter(distanceAdapter);
+        binding.distanceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                distanceScale = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        ArrayAdapter<CharSequence> landmarkAdapter = ArrayAdapter.createFromResource(getContext(), R.array.landmarks, android.R.layout.simple_spinner_item);
+        landmarkAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        binding.landmarksSpinner.setAdapter(landmarkAdapter);
+        binding.landmarksSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                landmarksType = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        binding.saveSettingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), distanceScale +" " +landmarksType, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
         return root;
     }
 
